27134c82d55503786bc497b8fff30a2ed2a5917b
blopasp <77591670+blopasp@users.noreply.github.com>
2021-04-24 16:30:26 -0300

    Update regiao.py

--- Filiais/regiao.py
+++ Filiais/regiao.py
@@ -6,35 +6,14 @@ import baseFiliais as bf
 
 # Query regiao
 regiao = """
-SELECT
-	FL.FILI_CD_FILIAL Filial,
-	MN.MUNI_NM_MUNICIPIO AS Cidade,
-	MN.ESTA_SG_UF AS UF,
-	EST.REGIAO as Regiao,
-	FILI_CD_BRICK AS BRICK
-
-
-FROM FILIAL FL
-
-LEFT JOIN MUNICIPIO MN
-ON MN.MUNI_SG_MUNICIPIO = FL.MUNI_SG_MUNICIPIO
-AND FL.ESTA_SG_UF = MN.ESTA_SG_UF 
-LEFT JOIN DBO.ESTADO EST (NOLOCK)
-ON MN.ESTA_SG_UF = EST.ESTA_SG_UF
-
-GROUP BY
-FL.FILI_CD_FILIAL,
-	MN.ESTA_SG_UF,
-	MN.MUNI_NM_MUNICIPIO,
-	FILI_CD_BRICK,
-	EST.REGIAO
+consulta
 """
 #Fazendo consulta no banco de dados
 
 def consulta(query):
     
-    server = 'cosmos'
-    database = 'cosmos_v14b'
+    server = server
+    database = database
     
     con = o.connect('DRIVER={SQL SERVER};SERVER='+server+';DATABASE='+database)
     cursor = con.cursor()
